6090e7ea4e3cb2d64647a14858b1b45f691ee668
victor ordax <victorordaxantolin@gmail.com>
2020-02-01 22:21:15 +0100

    primer componente

---
+++ aplicacionFutbol/src/store.js
@@ -0,0 +1,17 @@
+import { createStore } from  "redux"
+
+
+const initialState = {
+    jugadores:[],
+    titulares:[],
+    suplentes:[]
+}
+
+
+const reducerEntrenador =(state = initialState, action) =>{
+    return state
+}
+
+
+
+export default createStore(reducerEntrenador)
